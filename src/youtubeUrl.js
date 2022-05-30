dbb9ac3ae2f66b9dcf58f64f9a6f988f6773e587
Prabhat Jadhav <prabhatjadhav.exe@gmail.com>
2022-05-30 19:56:02 +0530

    reversed third commit

--- src/youtubeUrl.js
+++ src/youtubeUrl.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+const KEY = "AIzaSyBI9utABVMX7VUCoUuYC6uIPHrGSxbhjB8";
+
 const youtubeUrl = axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3/",
   params: {
     part: "snippet",
     maxResults: 5,
-    key: `${process.env.REACT_APP_YT_KEY}`,
+    key: KEY,
   },
 });
 
